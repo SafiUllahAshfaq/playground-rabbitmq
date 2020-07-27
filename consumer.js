34739ec522f8f035a1a0b579601fe6f086c4371b
SafiUllahAshfaq <safiu799@gmail.com>
2020-07-27 19:03:54 +0500

    publisher subscriber in place

---
+++ consumer.js
@@ -0,0 +1,25 @@
+const amqp = require('amqplib');
+
+const msg = { number: 2 };
+
+connect();
+
+async function connect() {
+	try {
+		const connStr = 'amqp://localhost:5672';
+
+		const conn = await amqp.connect(connStr);
+
+		const channel = await conn.createChannel();
+
+		const result = await channel.assertQueue('jobs');
+
+		channel.consume('jobs', (msg) => {
+			const input = JSON.parse(msg.content.toString(), undefined, 2);
+
+			console.log(input);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
---
+++ publisher.js
@@ -0,0 +1,23 @@
+const amqp = require('amqplib');
+
+const msg = { number: 2 };
+
+connect();
+
+async function connect() {
+	try {
+		const connStr = 'amqp://localhost:5672';
+
+		const conn = await amqp.connect(connStr);
+
+		const channel = await conn.createChannel();
+
+		const result = await channel.assertQueue('jobs');
+
+		channel.sendToQueue('jobs', Buffer.from(JSON.stringify(msg)));
+
+		console.log('Job sent successfully!! ', msg);
+	} catch (error) {
+		console.error(error);
+	}
+}
